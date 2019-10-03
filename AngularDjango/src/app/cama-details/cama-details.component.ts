e2fe9cfb384e681dc2e16e8db6c53358fedb7cf7
laryan <laryangv@gmail.com>
2019-10-03 03:31:04 -0300

    update general

---
+++ AngularDjango/src/app/cama-details/cama-details.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { CamaService } from '../cama.service';
+import { Cama } from '../cama';
+
+import { CamasListComponent } from '../camas-list/camas-list.component';
+
+
+
+@Component({
+  selector: 'cama-details',
+  templateUrl: './cama-details.component.html',
+  styleUrls: ['./cama-details.component.css']
+})
+export class CamaDetailsComponent implements OnInit {
+
+  @Input() cama: Cama;
+
+  constructor(private camaService: CamaService, private listComponent: CamasListComponent) { }
+
+  ngOnInit() {
+  }
+
+  updateActive(isActive: boolean) {
+    this.camaService.updateCama(this.cama.id,
+      { codigo: this.cama.codigo, name: this.cama.name, age: this.cama.age, 
+        enfermedad: this.cama.enfermedad, estado: this.cama.estado,
+        aislamiento: this.cama.aislamiento, active: isActive })
+      .subscribe(
+        data => {
+          console.log(data);
+          this.cama = data as Cama;
+        },
+        error => console.log(error));
+  }
+ 
+  deleteCama() {
+    this.camaService.deleteCama(this.cama.id)
+      .subscribe(
+        data => {
+          console.log(data);
+          this.listComponent.reloadData();
+        },
+        error => console.log(error));
+  }
+
+}
